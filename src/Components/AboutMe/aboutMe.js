863881b6abf6fd3f6bc6169e0a31d932c05a6252
Sahiba K <khanna.sahiba@gmail.com>
2020-01-22 17:34:26 -0500

    updated about me

--- src/Components/AboutMe/aboutMe.js
+++ src/Components/AboutMe/aboutMe.js
@@ -15,12 +15,14 @@ export default class AboutMe extends React.Component{
                         <a href="https://github.com/sahibak" target="_blank" rel="noopener noreferrer"><img src={Github} alt="GitHub logo"/></a>
                     </ul>
                 </article>
+                <article class>
                 <ul className="self-description">
                     <li className="self-description-point">I am Sahiba Khanna, a new front-end developer. I am a tenatious problem solver with passion for React, CSS/SCSS, JavaScript.</li>
                     <li className="self-description-point">I have previous expereince in data analysis, product ownernership, business developement, strategy and planning. Playing around with coding as my new year resolution for 2019, got me thinking about it as a career choice.</li>
                     <li className="self-description-point">I decided to make a forward leaning decission - to learn web development as my first step in the direction of pursuing my new found interest. By the time I completed my bootcamp at BrainStation (Toronto), it was evident the love for coding had become a passion.</li>
                     <li className="self-description-point"> I hope you enjoy viewing my projects as much as I enjoyed making each one of them.</li>
                 </ul>
+                </article>
             </section>
         )
     }
