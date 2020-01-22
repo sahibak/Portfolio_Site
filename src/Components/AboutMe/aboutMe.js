7cb2b0b6beb4b5045784e96d31fa3bfff994960d
Sahiba K <khanna.sahiba@gmail.com>
2020-01-22 18:22:00 -0500

    updated about me with email details

--- src/Components/AboutMe/aboutMe.js
+++ src/Components/AboutMe/aboutMe.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./aboutMe.scss";
 import Linkedin from "../../Assets/linkedin.svg";
 import Github from "../../Assets/github.svg";
+import Mail from "../../Assets/mail.svg";
 
 export default class AboutMe extends React.Component{
     render() {
@@ -13,16 +14,14 @@ export default class AboutMe extends React.Component{
                     <ul className="self-links">
                         <a href="https://www.linkedin.com/in/sahibak" target="_blank" rel="noopener noreferrer"><img src={Linkedin} alt="LinkedIn logo"/></a>
                         <a href="https://github.com/sahibak" target="_blank" rel="noopener noreferrer"><img src={Github} alt="GitHub logo"/></a>
+                        <a href="mailto:reach.sahibak@gmail.com" target="_blank" rel="noopener noreferrer"><img src={Mail} alt="Email"/></a>
                     </ul>
                 </article>
-                <article class>
                 <ul className="self-description">
                     <li className="self-description-point">I am Sahiba Khanna, a new front-end developer. I am a tenatious problem solver with passion for React, CSS/SCSS, JavaScript.</li>
                     <li className="self-description-point">I have previous expereince in data analysis, product ownernership, business developement, strategy and planning. Playing around with coding as my new year resolution for 2019, got me thinking about it as a career choice.</li>
-                    <li className="self-description-point">I decided to make a forward leaning decission - to learn web development as my first step in the direction of pursuing my new found interest. By the time I completed my bootcamp at BrainStation (Toronto), it was evident the love for coding had become a passion.</li>
-                    <li className="self-description-point"> I hope you enjoy viewing my projects as much as I enjoyed making each one of them.</li>
+                    <li className="self-description-point"> For any questions, assitance, business opportunities please contact via email/LinkedIn.</li>
                 </ul>
-                </article>
             </section>
         )
     }
