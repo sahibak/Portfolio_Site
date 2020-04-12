8f17b975a1bd219bf287a92eb8e42f26605f00ed
Sahiba K <khanna.sahiba@gmail.com>
2020-04-12 11:00:30 -0400

    added data structures

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./header.scss";
 
-export default class Header extends React.Component{
-    render() {
-        return (
-            <nav className="header">
-            <section className="header-details">
-                <h1 className="header-name">Sahiba Khanna</h1>
-                <h2 className="header-title">FRONTEND DEVELOPER</h2>
-            </section>
-            </nav>
-            
-        )
-    }
+export default class Header extends React.Component {
+  render() {
+    return (
+      <nav className="header">
+        <section className="header-details">
+          <h1 className="header-name">Sahiba Khanna</h1>
+          <h2 className="header-title">FULLSTACK DEVELOPER</h2>
+        </section>
+      </nav>
+    );
+  }
 }
--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -3,56 +3,68 @@ import Project from "../Project/project.js";
 import "./projects.scss";
 import Portfolio from "../../Assets/portfolio.png";
 import TickTackToe from "../../Assets/tickTackToe.png";
-import Grosure from "../../Assets/grosure.png"
+import DataStructures from "../../Assets/dataStructures.jpg";
+import Grosure from "../../Assets/grosure.png";
 
-export default class Projects extends React.Component{
-    // array of the projects I have worked on.
-    allProjects = [
-        {
-            "name": "My Portfolio",
-            "description": "Developement of my portoflio website to share my develpoment journey.",
-            "image": {Portfolio},
-            "link": "https://github.com/sahibak/Portfolio_Site"
-        },
-        {
-            "name": "Grosure",
-            "description": "BrainStation Graduation Capstone - A web application to help with recipe selection and preparing a grocery list based on requirements.",
-            "image": {Grosure},
-            "link":"https://github.com/sahibak/Capstone"
-        },
-        {
-            "name": "Tick-Tack-Toe",
-            "description": "First pair-programming project at BrainStation, in collobaration with Michael Beaudry.",
-            "image": {TickTackToe},
-            "link":"https://github.com/sahibak/ca-pair-mike-sahiba"
-        }
-    ]
-                
-    //Function to render all individual projects with thier details. 
-    creatingProjectList(){
-        let projectList = [];
-        for (let i=0; i < this.allProjects.length; i++ ){
-            let { name, description, image, link } = this.allProjects[i];
-            projectList.push(
-                <Project key={i} 
-                        projectName={name}
-                        projectDescription = {description}
-                        projectImage = {Object.values(image)[0]}
-                        projectLink = {link}>
-                </Project>
-            );
-        }
-        return projectList
+export default class Projects extends React.Component {
+  // array of the projects I have worked on.
+  allProjects = [
+    {
+      name: "Data Structures",
+      description:
+        "Learning data structures to solve everyday problems in computing.",
+      image: { DataStructures },
+      link: "https://github.com/sahibak/algorithms"
+    },
+    {
+      name: "My Portfolio",
+      description:
+        "Developement of my portoflio website to share my develpoment journey.",
+      image: { Portfolio },
+      link: "https://github.com/sahibak/Portfolio_Site"
+    },
+    {
+      name: "Grosure",
+      description:
+        "BrainStation Graduation Capstone - A web application to help with recipe selection and preparing a grocery list based on requirements.",
+      image: { Grosure },
+      link: "https://github.com/sahibak/Capstone"
+    },
+    {
+      name: "Tick-Tack-Toe",
+      description:
+        "First pair-programming project at BrainStation, in collobaration with Michael Beaudry.",
+      image: { TickTackToe },
+      link: "https://github.com/sahibak/ca-pair-mike-sahiba"
     }
+  ];
 
-    render() {
-        return (
-            <section className="projects"> 
-                <h2 className="projects-title">Projects</h2>
-                <article className="projects-list">
-                    {this.creatingProjectList()}
-                </article>
-            </section>
-        )
+  //Function to render all individual projects with thier details.
+  creatingProjectList() {
+    let projectList = [];
+    for (let i = 0; i < this.allProjects.length; i++) {
+      let { name, description, image, link } = this.allProjects[i];
+      projectList.push(
+        <Project
+          key={i}
+          projectName={name}
+          projectDescription={description}
+          projectImage={Object.values(image)[0]}
+          projectLink={link}
+        ></Project>
+      );
     }
+    return projectList;
+  }
+
+  render() {
+    return (
+      <section className="projects">
+        <h2 className="projects-title">Projects</h2>
+        <article className="projects-list">
+          {this.creatingProjectList()}
+        </article>
+      </section>
+    );
+  }
 }
